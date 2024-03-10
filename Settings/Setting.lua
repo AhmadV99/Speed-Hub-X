4e87283285ccc8fdce039eef7457ad2180c55d92
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2024-03-10 21:02:11 +0400

    Create Setting.lua

---
+++ Settings/Setting.lua
@@ -0,0 +1,9 @@
+-- [[ Local ]] --
+local DiscordLink = "https://discord.gg/tusaQzURPQ"
+local YoutubeLink = "https://www.youtube.com/@TMRXofficial"
+-- [[ _G. ]] --
+_G.Discord = DiscordLink
+_G.Youtube = YoutubeLink
+-- [[ getgenv ]] --
+getgenv().Discord = DiscordLink
+getgenv().Youtube = YoutubeLink
