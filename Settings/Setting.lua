90c509669c25bb5397044dd6dac5f370e6c6fb45
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2024-03-10 21:14:20 +0400

    Update Setting.lua

--- Settings/Setting.lua
+++ Settings/Setting.lua
@@ -7,3 +7,21 @@ _G.Youtube = YoutubeLink
 -- [[ getgenv ]] --
 getgenv().Discord = DiscordLink
 getgenv().Youtube = YoutubeLink
+
+if getgenv().FPSMAX or _G.FPSMAX then
+	spawn(function()
+		pcall(function()
+			loadstring([[
+				local RS = game:GetService("RunService").RenderStepped
+				local function GetFPS() 
+					RS:Wait() 
+					local V = 0 for _ = 1, 5 do 
+						V += 1 / RS:Wait() 
+					end 
+					return math.round(V / 5) 
+				end
+				pcall(function() setfpscap(GetFPS()) end)
+			]])()
+		end)
+	end)
+end
