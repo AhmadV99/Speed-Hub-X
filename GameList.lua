63403955678a8459c0539b2878af10a4ef76f62f
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2024-06-04 15:06:05 +0400

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,5 @@
 getgenv().Games = {
-  [2753915549] = "" -- Blox Fruit Sea 1
-  [4442272183] = "" -- Blox Fruit Sea 2
-  [7449423635] = "" -- Blox Fruit Sea 3
+  [2753915549] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua" -- Blox Fruit Sea 1
+  [4442272183] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua" -- Blox Fruit Sea 2
+  [7449423635] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua" -- Blox Fruit Sea 3
 }
