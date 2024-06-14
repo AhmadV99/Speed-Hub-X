7525d23d897ce1297ea3d738cfb49b9cc62ace3a
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2024-06-14 22:27:23 +0400

    Update Speed Hub X.lua

--- Speed Hub X.lua
+++ Speed Hub X.lua
@@ -1,11 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
-if identifyexecutor():find("Arecus X") or identifyexecutor():find("Codex") then
-    for Index, Value in next, debug do
-        if not getgenv()[Index] then
-            getgenv()[Index] = Value
-        end
-    end
-end
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
