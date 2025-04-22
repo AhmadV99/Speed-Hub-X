76c54c766872a70cf4e662189f50a2612c575e33
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2025-04-23 00:21:34 +0400

    Update Settings.lua

--- Settings.lua
+++ Settings.lua
@@ -1,7 +1,7 @@
 local _env = getgenv and getgenv() or {}
 
 local Ver = "Version: 3.7.0"
-local Dis = "https://discord.gg/speedhubx"
+local Dis = "https://discord.gg/UXvCpyJ6"
 
 _env.Discord = Dis
 _env.Version = Ver
