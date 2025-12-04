06e4381346424f67ed2908e70dc01bc6ff8318e0
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2025-12-04 18:27:06 +0400

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -8,6 +8,7 @@ local Games = {
   [7326934954] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/99%20Nights%20in%20the%20Forest.lua", -- 99 Nights in the Forest
   [6701277882] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Fish%20It.lua", -- Fish It
   [8316902627] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Plant%20Vs%20Brainrot.lua", -- Plant Vs Brainrot
+  [7671049560] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/The%20Forge.lua", -- The Forge
 }
 
 return Games
