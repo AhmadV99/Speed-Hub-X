2190bc4d990798531127fa74688e7607b73dcc58
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2026-04-05 19:14:59 +0400

    Update Speed Hub X.lua

--- Speed Hub X.lua
+++ Speed Hub X.lua
@@ -1,7 +1,17 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
+--!strict
 
-local URL = Games[game.GameId]
+local HttpGet = game.HttpGet
+local GameId: number = game.GameId
 
-if URL then
-  loadstring(game:HttpGet(URL))()
-end
+local Games: {[number]: string} = loadstring(
+  HttpGet(game, "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua")
+)() :: any
+
+local URL: string? = Games[GameId]
+if not URL then return end
+
+task.spawn(function()
+  local Loader = loadstring(HttpGet(game, URL))()
+  
+  return Loader
+end)
